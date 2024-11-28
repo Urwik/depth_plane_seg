8e660b71491cb09e66a4dd5a0e8a960d88370502
FJSoler <f.soler@umh.es>
2024-11-28 11:40:28 +0000

    remove cached

--- multi_source.sh
+++
@@ -1,4 +0,0 @@
-source /opt/ros/galactic/setup.bash
-source /home/fran/workspaces/isec/galactic_ws/install/local_setup.bash
-source /usr/share/colcon_cd/function/colcon_cd.sh
-source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
